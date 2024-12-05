f8e1ceab3b29c0d1128c7214710468e61e856294
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2024-12-05 13:19:21 +0100

    Create change_url.py

---
+++ examples/change_url.py
@@ -0,0 +1,28 @@
+from PyChromeController import PyChromeController
+import time
+
+def test():
+    # PyChromeController initialisieren
+    controller = PyChromeController()
+
+    controller.start_browser_session()
+
+    # Browser starten und Tabs öffnen
+    controller.open_url("https://www.example.com")
+    controller.open_new_tab("https://www.google.com")
+    controller.open_new_tab("https://www.github.com")
+    controller.open_url("https://facebook.com")
+
+    time.sleep(3)
+
+    controller.change_url_of_tab("https://www.instagram.com", 1)
+
+    time.sleep(2)
+  
+    controller.change_url_of_tab_by_url("https://facebook.com", "https://www.youtube.com/")
+
+    time.sleep(2)
+
+    controller.change_url_of_tab_by_title("Domain", "https://web.whatsapp.com/")
+
+test()
