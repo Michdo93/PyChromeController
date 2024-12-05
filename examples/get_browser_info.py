4b499171b33ffc80829061aa3dd790fac043e862
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2024-12-05 13:03:15 +0100

    Create get_browser_info.py

---
+++ examples/get_browser_info.py
@@ -0,0 +1,14 @@
+from PyChromeController import PyChromeController
+
+def test():
+    # PyChromeController initialisieren
+    controller = PyChromeController()
+
+    controller.start_browser_session()
+
+    # Browser starten und Tabs öffnen
+    controller.open_url("https://www.example.com")
+
+    print(controller.get_browser_info())
+
+test()
