c1cabb504a5260765581aba89a331d01fa794d6e
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2024-12-05 13:00:22 +0100

    Create check_and_open_tabs.py

---
+++ examples/check_and_open_tabs.py
@@ -0,0 +1,36 @@
+from PyChromeController import PyChromeController
+import time
+
+def test():
+    # PyChromeController initialisieren
+    controller = PyChromeController()
+
+    controller.start_browser_session()
+
+    # Browser starten und Tabs öffnen
+    controller.open_url("https://www.example.com")
+    controller.check_and_open_tab_by_url("https://www.google.com")
+    controller.check_and_open_tab_by_url("https://www.github.com")
+    controller.check_and_open_tab_by_url("https://facebook.com")
+
+    time.sleep(3)
+
+    controller.check_and_open_tab_by_url("https://www.google.com")
+
+    time.sleep(1)
+  
+    controller.check_and_open_tab_by_url("https://www.github.com")
+
+    time.sleep(1)
+
+    controller.check_and_open_tab_by_url("https://facebook.com")
+
+    time.sleep(1)
+
+    check_and_open_tab_by_title("Domain", "https://www.example.com")
+
+    time.sleep(1)
+
+    check_and_open_tab_by_title("YouTube", "https://www.youtube.com/")
+
+test()
